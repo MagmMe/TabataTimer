8f3da839cfa9c6206d34ccf5f40b8e9807920b3e
MagmMe <marcin@magm.me>
2020-05-02 12:19:25 +0200

    creating roundshape timer

--- TabataTimer PalkaTeam/ViewController.swift
+++ TabataTimer PalkaTeam/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
+        //    Drawing a circle
 
+        let center = view.center
+        
+        // Create track layer
+        let trackLayer = CAShapeLayer()
+        let circularPath = UIBezierPath( arcCenter: center, radius: 150, startAngle: -CGFloat.pi / 2, endAngle:  2 * CGFloat.pi, clockwise: true )
+               trackLayer.path = circularPath.cgPath
+               trackLayer.strokeColor = UIColor.lightGray.cgColor
+               trackLayer.lineWidth = 10
+               trackLayer.fillColor = UIColor.clear.cgColor
+               trackLayer.lineCap = .round
+        view.layer.addSublayer(trackLayer)
+        
+//        let circularPath = UIBezierPath( arcCenter: center, radius: 150, startAngle: -CGFloat.pi / 2, endAngle:  2 * CGFloat.pi, clockwise: true )
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineCap = .round
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc func handleTap(){
+        print("Tappin is working right")
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 10
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
+    
 
 }
 
